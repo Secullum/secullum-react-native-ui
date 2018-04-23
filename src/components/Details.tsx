673d5949e2c5f21fdc8011738dd09efa683dae3f
fhelwanger <fhelwanger@gmail.com>
2018-04-23 11:05:14 -0300

    Add Details

---
+++ src/components/Details.tsx
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Card } from './Card';
+import { Space } from './Space';
+import { getTheme } from '../modules/theme';
+
+export interface DetailsIcon {
+  name: string;
+  color: string;
+  size: number;
+  onPress?: () => void;
+}
+
+export interface DetailsLine {
+  title?: string;
+  value?: string;
+  icons?: Array<DetailsIcon>;
+  render?: () => React.ReactNode;
+}
+
+export interface DetailsProperties {
+  title: string;
+  lines: Array<DetailsLine>;
+}
+
+export class Details extends React.Component<DetailsProperties> {
+  renderIcon = (icon: DetailsIcon, index: number) => {
+    const fa = (
+      <FontAwesome
+        key={index}
+        name={icon.name}
+        color={icon.color}
+        size={icon.size}
+        style={styles.lineValueIcon}
+      />
+    );
+
+    if (icon.onPress) {
+      return (
+        <TouchableOpacity key={index} onPress={icon.onPress}>
+          {fa}
+        </TouchableOpacity>
+      );
+    }
+
+    return fa;
+  };
+
+  renderLine = (line: DetailsLine, index: number) => {
+    const { title, value, icons, render } = line;
+
+    const sectionStyle = render
+      ? styles.lineSectionCustomRender
+      : styles.lineSection;
+
+    const children = render ? (
+      render()
+    ) : (
+      <>
+        <Text style={styles.lineTitle}>{title}</Text>
+        <View style={styles.lineValueContainer}>
+          {icons && icons.map(this.renderIcon)}
+          <Text style={styles.lineTitle}>{value}</Text>
+        </View>
+      </>
+    );
+
+    return (
+      <React.Fragment key={index}>
+        <Space height={3} />
+        <Card>
+          <Card.Section style={sectionStyle}>{children}</Card.Section>
+        </Card>
+      </React.Fragment>
+    );
+  };
+
+  render() {
+    const { title, lines } = this.props;
+
+    return (
+      <>
+        <Card>
+          <Card.Section>
+            <Text style={styles.title}>{title}</Text>
+          </Card.Section>
+        </Card>
+        {lines.map(this.renderLine)}
+      </>
+    );
+  }
+}
+
+const theme = getTheme();
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 18,
+    color: theme.textColor1
+  },
+  lineSection: {
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 45
+  },
+  lineSectionCustomRender: {
+    padding: 0
+  },
+  lineTitle: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 14,
+    color: theme.textColor1,
+    minWidth: 40,
+    textAlign: 'center'
+  },
+  lineValueContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  lineValueIcon: {
+    width: 25,
+    textAlign: 'center'
+  }
+});
---
+++ src/components/DetailsHeader.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Card } from './Card';
+import { getTheme } from '../modules/theme';
+
+export interface DetailsHeaderProperties {
+  text: string;
+  onLeftPress?: () => void;
+  onRightPress?: () => void;
+}
+
+export class DetailsHeader extends React.Component<DetailsHeaderProperties> {
+  renderButton = (
+    type: 'left' | 'right',
+    onPress: (() => void) | undefined
+  ) => {
+    if (onPress) {
+      return (
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <FontAwesome
+            name={type === 'left' ? 'angle-left' : 'angle-right'}
+            color={theme.textColor3}
+            size={14}
+          />
+        </TouchableOpacity>
+      );
+    }
+
+    return <View style={styles.buttonPlaceholder} />;
+  };
+
+  render() {
+    const { text, onLeftPress, onRightPress } = this.props;
+
+    return (
+      <Card>
+        <Card.Section style={styles.container}>
+          {this.renderButton('left', onLeftPress)}
+          <Text style={styles.text}>{text}</Text>
+          {this.renderButton('right', onRightPress)}
+        </Card.Section>
+      </Card>
+    );
+  }
+}
+
+const theme = getTheme();
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 18,
+    color: theme.textColor1
+  },
+  button: {
+    height: 24,
+    width: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: theme.borderColor1,
+    borderWidth: 1,
+    borderRadius: 24
+  },
+  buttonPlaceholder: {
+    height: 24,
+    width: 24
+  }
+});
