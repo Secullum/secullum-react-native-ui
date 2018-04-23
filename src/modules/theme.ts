136dfca75ffdc42dfdef90ea6d014a8d413c9c7c
fhelwanger <fhelwanger@gmail.com>
2018-04-23 02:20:12 -0300

    Initial commit

---
+++ src/modules/theme.ts
@@ -0,0 +1,16 @@
+export const getTheme = () => {
+  return {
+    textColor1: '#282c37', // black
+    textColor2: '#0c65c7', // blue
+    textColor3: '#8c9aa8', // gray
+    textColor4: '#ffffff', // white
+    backgroundColor1: '#ffffff', // white
+    backgroundColor2: '#eef1f6', // gray
+    backgroundColor3: '#0c65c7', // blue
+    borderColor1: '#c2c2c2', // gray
+    statusBarColor: '#0047a9', // blue
+    successColor: '#2ec562', // green
+    warningColor: '#f5a623', // yellow
+    errorColor: '#d62729' // red
+  };
+};
