0473ef2716f35080e26469969e51cbb5f0a3911e
Guilherme Arui Appel <gappel@secullum.local>
2018-07-04 17:38:17 -0300

    Translation fixes.

--- src/modules/format.ts
+++ src/modules/format.ts
@@ -1,7 +1,20 @@
 import { format as dfnsFormat } from 'date-fns';
 
-const locales = {
-  pt: require('date-fns/locale/pt')
+let locale: string = 'pt';
+
+export const setLocale = (loc: string) => {
+  locale = loc;
+};
+
+const getLocale = () => {
+  switch (locale) {
+    case 'pt':
+      return require('date-fns/locale/pt');
+    case 'es':
+      return require('date-fns/locale/es');
+    default:
+      return require('date-fns/locale/en');
+  }
 };
 
 const fixWeekdayLowercase = (formattedDate: string) => {
@@ -24,6 +37,37 @@ const fixWeekdayLowercase = (formattedDate: string) => {
   return formattedDate;
 };
 
+const fixEspanishWeekdayLowercase = (formattedDate: string) => {
+  formattedDate = formattedDate.replace('lunes', 'Lunes');
+  formattedDate = formattedDate.replace('martes', 'Martes');
+  formattedDate = formattedDate.replace('miércoles', 'Miércoles');
+  formattedDate = formattedDate.replace('jueves', 'Jueves');
+  formattedDate = formattedDate.replace('viernes', 'Viernes');
+  formattedDate = formattedDate.replace('sábado', 'Sábado');
+  formattedDate = formattedDate.replace('domingo', 'Domingo');
+
+  formattedDate = formattedDate.replace('lun', 'Lun');
+  formattedDate = formattedDate.replace('mar', 'Mar');
+  formattedDate = formattedDate.replace('mié', 'Mié');
+  formattedDate = formattedDate.replace('jue', 'Jue');
+  formattedDate = formattedDate.replace('vie', 'Vie');
+  formattedDate = formattedDate.replace('sáb', 'Sáb');
+  formattedDate = formattedDate.replace('dom', 'Dom');
+
+  return formattedDate;
+};
+
 export const formatDate = (date: Date, format: string) => {
-  return fixWeekdayLowercase(dfnsFormat(date, format, { locale: locales.pt }));
+  switch (locale) {
+    case 'pt':
+      return fixWeekdayLowercase(
+        dfnsFormat(date, format, { locale: getLocale() })
+      );
+    case 'es':
+      return fixEspanishWeekdayLowercase(
+        dfnsFormat(date, format, { locale: getLocale() })
+      );
+    default:
+      return dfnsFormat(date, format, { locale: getLocale() });
+  }
 };
