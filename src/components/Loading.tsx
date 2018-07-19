292ef10a5bcb4dade712790551c137eb3ef36a33
diegoport <diegoportteixeira@gmail.com>
2018-07-19 17:27:35 -0300

    update loadind layout

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,18 +4,26 @@ import { ActivityIndicator, StyleSheet, View } from 'react-native';
 export class Loading extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <ActivityIndicator size="large" />
-      </View>
+      <>
+        <View style={styles.backgroundView} />
+        <ActivityIndicator style={styles.indicator} size="large" color="#aaa" />
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10
+  backgroundView: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    opacity: 0.6,
+    backgroundColor: '#ccc'
+  },
+  indicator: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'space-around'
   }
 });
