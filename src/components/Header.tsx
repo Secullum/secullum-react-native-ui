6da1d0adeafc79f99f7455f6a512c543d9cfef74
Fernando Alex Helwanger <fhelwanger@gmail.com>
2018-11-27 11:57:38 +0100

    Update src/components/Header.tsx
    Co-Authored-By: daminebenz <33185490+daminebenz@users.noreply.github.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ export class Header extends React.Component<HeaderProperties> {
     const styles = StyleSheet.create({
       header: {
         backgroundColor: theme.backgroundColor3,
-        height: Header.height || isTablet() ? 60 : 40,
+        height: Header.height,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center'
