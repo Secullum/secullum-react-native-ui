183e256e90e30ec1eaed2b29a06e71c83a53e337
fhelwanger <fhelwanger@gmail.com>
2019-04-24 14:07:58 -0300

    Remove unnecessary typings

--- types/react-date-range/index.d.ts
+++ types/react-date-range/index.d.ts
@@ -22,38 +22,3 @@ declare module 'react-date-range' {
 
   export class DateRange extends React.Component<DateRangeProps> {}
 }
-
-declare module 'react-date-range/dist/locale' {
-  type Locale = {
-    formatDistance: Function;
-    formatRelative: Function;
-    localize: {
-      ordinalNumber: Function;
-      era: Function;
-      quarter: Function;
-      month: Function;
-      day: Function;
-      dayPeriod: Function;
-    };
-    formatLong: Object;
-    date: Function;
-    time: Function;
-    dateTime: Function;
-    match: {
-      ordinalNumber: Function;
-      era: Function;
-      quarter: Function;
-      month: Function;
-      day: Function;
-      dayPeriod: Function;
-    };
-    options?: {
-      weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-      firstWeekContainsDate?: 1 | 2 | 3 | 4 | 5 | 6 | 7;
-    };
-  };
-
-  export const pt: Locale;
-  export const es: Locale;
-  export const enUS: Locale;
-}
