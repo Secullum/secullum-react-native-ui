e8952e7d783d31d54f8143c5be971020373a72b9
fghiggi <felipeghiggi92@gmail.com>
2019-06-24 17:06:24 -0300

    Current selected menu fixed on MenuDesktop

--- src/components/MenuDesktop.tsx
+++ src/components/MenuDesktop.tsx
@@ -17,7 +17,7 @@ export interface MenuDesktopProperties {
     subText?: string;
   }>;
   onMenuPress: (path: string) => void;
-  currentMenuPath?: string;
+  isCurrentMenuPath?: (path: string) => boolean;
   headerHeight: number;
 }
 
@@ -60,13 +60,13 @@ export class MenuDesktop extends React.Component<MenuDesktopProperties> {
   };
 
   render() {
-    const { menu, onMenuPress, currentMenuPath } = this.props;
+    const { menu, onMenuPress, isCurrentMenuPath } = this.props;
     const styles = this.getStyles();
 
     return (
       <View style={styles.container}>
         {menu.map(menuItem => {
-          const selected = menuItem.path === currentMenuPath;
+          const selected = isCurrentMenuPath!(menuItem.path);
 
           const textStyles = [
             styles.itemText,
