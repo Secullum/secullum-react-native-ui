d5d2418ccf37a5ea51adf69338600e932d9619af
fghiggi <felipeghiggi92@gmail.com>
2019-07-29 14:31:20 -0300

    Adding Text component

---
+++ src/components/Text.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Text as TextNative, StyleProp, TextStyle } from 'react-native';
+import { getTheme } from '../modules/theme';
+
+export interface TextProperties {
+  style?: StyleProp<TextStyle>;
+  bold: boolean;
+  size: number;
+  flex?: number;
+  color: string;
+  align?: 'auto' | 'left' | 'right' | 'center' | 'justify';
+}
+
+const theme = getTheme();
+
+export class Text extends React.Component<TextProperties> {
+  static defaultProps = {
+    bold: false,
+    size: 14,
+    color: theme.textColor1
+  };
+
+  render() {
+    const { bold, color, size, style, flex } = this.props;
+    return (
+      <TextNative
+        style={[
+          {
+            color,
+            flex,
+            fontSize: size,
+            fontFamily: bold ? 'Lato-Bold' : 'Lato-Regular'
+          },
+          style
+        ]}
+      >
+        {this.props.children}
+      </TextNative>
+    );
+  }
+}
