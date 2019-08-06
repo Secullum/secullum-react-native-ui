0f17a083befb429b35f291bbf9477e4da1e98934
fghiggi <felipeghiggi92@gmail.com>
2019-08-06 08:56:22 -0300

    Fix TextBoxMask for web

--- src/components/TextBoxMask.tsx
+++ src/components/TextBoxMask.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { TextBox, TextBoxProperties } from './TextBox';
+import { Platform } from 'react-native';
 
 import {
   TextInputMask,
@@ -25,7 +26,10 @@ export class TextBoxMask extends React.Component<TextBoxMaskProperties> {
         renderInput={props => {
           const { ref, ...otherProps } = props;
 
-          return (
+          return Platform.OS === 'web' ? (
+            // @ts-ignore :Waiting for typing https://github.com/benhurott/react-native-masked-text/pull/74
+            <TextInputMask {...otherProps} type={type} options={options} />
+          ) : (
             // @ts-ignore :Waiting for typing https://github.com/benhurott/react-native-masked-text/pull/74
             <TextInputMask
               {...otherProps}
