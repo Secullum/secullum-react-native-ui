8ced9603980fdc4205a661fb8e5c540a106a3c1c
fhelwanger <fhelwanger@gmail.com>
2019-10-30 09:01:03 -0300

    Format

--- src/components/TimePicker.web.tsx
+++ src/components/TimePicker.web.tsx
@@ -110,7 +110,9 @@ export class TimePicker extends React.Component<TimePickerProperties> {
               }
             }}
             onBlur={() => {
-              value.length > 0 && this.props.onChange(value + '00:00'.substr(value.length));
+              if (value.length > 0) {
+                this.props.onChange(value + '00:00'.substr(value.length));
+              }
             }}
           />
         </View>
