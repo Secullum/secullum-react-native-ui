c1fe766cf79f6969c892578874612d191b8e2eaa
paula.oliveira.m@gmail.com <paula.oliveira.m@gmail.com>
2020-03-04 07:48:58 -0300

    Expor o método onCancel do componente DateTimePickerModal.

--- src/components/TimePicker.tsx
+++ src/components/TimePicker.tsx
@@ -22,6 +22,7 @@ export interface TimePickerProperties {
   clearable?: boolean;
   disabled?: boolean;
   onChange?: (value: string) => void;
+  onCancel?:()=>void;
   style?: StyleProp<ViewStyle>;
   nativeID?: string;
 }
@@ -71,7 +72,12 @@ export class TimePicker extends React.Component<
   };
 
   handleCancel = () => {
-    this.setState({ showModal: false });
+    this.setState({showModal: false},()=>{
+      if (this.props.onCancel){
+        this.props.onCancel();
+      }
+    });
+
   };
 
   handleClear = () => {
