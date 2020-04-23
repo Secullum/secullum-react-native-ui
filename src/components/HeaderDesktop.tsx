c6d63cfc59dd081ca01c3b115906ed2fb9730d2f
paula.oliveira.m@gmail.com <paula.oliveira.m@gmail.com>
2020-04-23 13:24:16 -0300

    Including the ability of adding buttons to the right side of the HeaderDesktop component.

--- src/components/HeaderDesktop.tsx
+++ src/components/HeaderDesktop.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
 import { getTheme } from '../modules/theme';
+import { HeaderButton } from '../components/Header';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import {
   Image,
   ImageSourcePropType,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
+import { isTablet } from '../modules/layout';
 
 export interface HeaderDesktopProperties {
   logo: ImageSourcePropType;
   title: string;
   greeting?: string;
+  rightButton?: HeaderButton;
 }
 
 export class HeaderDesktop extends React.Component<HeaderDesktopProperties> {
@@ -47,14 +52,64 @@ export class HeaderDesktop extends React.Component<HeaderDesktopProperties> {
         fontFamily: 'Lato-Regular',
         marginLeft: 'auto',
         marginRight: 20
+      },
+      button: {
+        padding: isTablet() ? 14 : 10
+      },
+      counterContainer: {
+        position: 'absolute',
+        backgroundColor: theme.counterBackgroundColor,
+        height: isTablet() ? 20 : 15,
+        width: isTablet() ? 20 : 15,
+        top: 5,
+        left: 18,
+        borderRadius: 50
+      },
+      counterText: {
+        textAlign: 'center',
+        color: theme.counterTextColor,
+        fontSize: isTablet() ? 15 : 10
       }
     });
 
     return styles;
   };
 
+  renderRightButton = (button: HeaderButton) => {
+    const styles = this.getStyles();
+    const theme = getTheme();
+
+    const icon = (
+      <>
+        <FontAwesome
+          nativeID={button.nativeID}
+          name={button.icon}
+          size={isTablet() ? 30 : 20}
+          color={button.disabled ? theme.textColor1 : theme.textColor4}
+        />
+        {button.counter ? (
+          <View style={styles.counterContainer}>
+            <Text style={styles.counterText}>{button.counter}</Text>
+          </View>
+        ) : null}
+      </>
+    );
+
+    const style = [styles.button];
+
+    if (button.disabled) {
+      return <View style={style}>{icon}</View>;
+    }
+
+    return (
+      <TouchableOpacity onPress={button.onPress} style={style}>
+        {icon}
+      </TouchableOpacity>
+    );
+  };
+
   render() {
-    const { logo, title, greeting } = this.props;
+    const { logo, title, greeting, rightButton } = this.props;
     const styles = this.getStyles();
 
     return (
@@ -66,6 +121,7 @@ export class HeaderDesktop extends React.Component<HeaderDesktopProperties> {
             {greeting}
           </Text>
         )}
+        {rightButton && this.renderRightButton(rightButton)}
       </View>
     );
   }
