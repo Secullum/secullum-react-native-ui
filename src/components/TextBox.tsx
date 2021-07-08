378036401a7c3971c6a01874c0a3e8e70ec50f89
Rodrigo Weber <rodrigoantonioweber@hotmail.com>
2021-07-08 14:05:47 -0300

    Fixes to make it more generic and run on other software

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -94,7 +94,7 @@ export class TextBox extends React.Component<TextBoxProperties> {
         minHeight: 22,
         padding: 0,
         margin: 0,
-        flex: 2,
+        // flex: 2,
         ...(Platform.OS === 'web' ? { outline: '0' } : {})
       },
       readonly: {
--- src/modules/theme.ts
+++ src/modules/theme.ts
@@ -1,8 +1,24 @@
+export const BLACK = '#282c37';
+export const WHITE = '#ffffff';
+export const GRAY = '#8c9aa8';
+export const GRAY_1 = '#eef1f6';
+export const GRAY_2 = '#dddddd';
+export const GRAY_3 = '#c2c2c2';
+export const GRAY_4 = '#aabec6';
+export const BLUE = '#0047a9';
+export const BLUE_LIGHT = '#0c65c7';
+export const YELLOW = '#f5a623';
+export const GREEN = '#34bf6d';
+export const GREEN_LIGHT = '#2ec562';
+export const RED = '#d62729';
+
 export interface Theme {
   textColor1: string;
   textColor2: string;
   textColor3: string;
   textColor4: string;
+  messageTextColor: string;
+  counterTextColor: string;
   backgroundColor1: string;
   backgroundColor2: string;
   backgroundColor3: string;
@@ -14,29 +30,29 @@ export interface Theme {
   errorColor: string;
   disabledColor: string;
   counterBackgroundColor: string;
-  counterTextColor: string;
   fontFamily1: string;
   fontFamily2: string;
   fontFamily3: string;
 }
 
 let theme = {
-  textColor1: '#282c37', // black
-  textColor2: '#0c65c7', // blue
-  textColor3: '#8c9aa8', // gray
-  textColor4: '#ffffff', // white
-  backgroundColor1: '#ffffff', // white
-  backgroundColor2: '#eef1f6', // gray
-  backgroundColor3: '#0c65c7', // blue
-  borderColor1: '#c2c2c2', // gray
-  shadowColor1: '#aabec6', // gray
-  statusBarColor: '#0047a9', // blue
-  successColor: '#2ec562', // green
-  warningColor: '#f5a623', // yellow
-  errorColor: '#d62729', // red
-  disabledColor: '#dddddd', // gray
-  counterBackgroundColor: '#34bf6d', // light green
-  counterTextColor: '#ffffff', // white
+  textColor1: BLACK,
+  textColor2: BLUE_LIGHT,
+  textColor3: GRAY,
+  textColor4: WHITE,
+  messageTextColor: BLACK,
+  counterTextColor: WHITE,
+  backgroundColor1: WHITE,
+  backgroundColor2: GRAY_1,
+  backgroundColor3: BLUE_LIGHT,
+  borderColor1: GRAY_3,
+  shadowColor1: GRAY_4,
+  statusBarColor: BLUE,
+  successColor: GREEN_LIGHT,
+  warningColor: YELLOW,
+  errorColor: RED,
+  disabledColor: GRAY_2,
+  counterBackgroundColor: GREEN,
   fontFamily1: 'Roboto-Medium',
   fontFamily2: 'Roboto',
   fontFamily3: 'Roboto-Light'
