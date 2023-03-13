cd18eda943733d1629c04f17d2f69f011ce0c0fe
Elinton Henrique Ev <52543124+elintonev@users.noreply.github.com>
2023-03-13 17:05:03 -0300

    Fix dynamic island spacing to iOS (#118)
    * Fix dynamic island spacing to iOS
    
    * Adding comments

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,3 +1,4 @@
+import * as Device from 'expo-device';
 import * as React from 'react';
 import {
   StatusBar as ReactNativeStatusBar,
@@ -13,10 +14,28 @@ export class StatusBar extends React.Component {
   getStyles = () => {
     const theme = getTheme();
 
+    const dynamicHeight = () => {
+      const deviceId = Device.modelId;
+
+      // When is Android or Web, the modelId is null
+      // So we return the status bar height.
+      if (deviceId == null) {
+        return getStatusBarHeight();
+      }
+
+      const iPhoneId = deviceId.substring(6, 8);
+
+      // Dynamic Island is available on iPhone 14 Pro and 14 Pro max
+      // Here´s a list os iPhone´s IDs: https://www.theiphonewiki.com/wiki/Models
+      if (iPhoneId < 15) return getStatusBarHeight();
+
+      return getStatusBarHeight() + 25;
+    };
+
     const styles = StyleSheet.create({
       statusBar: {
         backgroundColor: theme.statusBarColor,
-        height: getStatusBarHeight()
+        height: dynamicHeight()
       }
     });
 
