a443eda17f73656fc578f0b57edeaeb5a7ac3bb0
Elinton Henrique Ev <52543124+elintonev@users.noreply.github.com>
2023-11-17 17:29:07 -0300

    Implementing subtotal in Table component to mobile (#129)
    * Implementing subtotal in Table component to mobile
    
    * Revisão

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -40,91 +40,98 @@ export interface TableProperties {
 }
 
 export class Table extends React.Component<TableProperties> {
+  renderHeaderColumn = (column: TableColumn) => {
+    const { data, onSelectAll } = this.props;
+
+    if (column.type === 'checkbox') {
+      return (
+        <CheckBox
+          key={column.key}
+          style={{ marginLeft: 8 }}
+          value={data.every(x => x.selected) || false}
+          onChange={value => onSelectAll && onSelectAll(value)}
+        />
+      );
+    }
+
+    return (
+      <Text
+        key={column.key}
+        style={[
+          styles.cell,
+          styles.cellHeader,
+          column.style,
+          column.headerStyle
+        ]}
+      >
+        {column.title}
+      </Text>
+    );
+  };
+
+  renderDataTable = (row: any, rowIndex: number) => {
+    const { idAttribute, columns, cellStyle, onSelect } = this.props;
+
+    return (
+      <View
+        key={row[idAttribute].toString()}
+        style={[
+          styles.row,
+          {
+            backgroundColor:
+              rowIndex % 2 === 0
+                ? theme.backgroundColor2
+                : theme.backgroundColor1
+          }
+        ]}
+      >
+        {columns.map(column => {
+          const style =
+            cellStyle &&
+            cellStyle[row[idAttribute].toString()] &&
+            cellStyle[row[idAttribute].toString()][column.key];
+
+          return column.type === 'icon' ? (
+            <FontAwesome
+              key={column.key}
+              name={row[column.key].toString()}
+              // @ts-ignore : The component uses a different version of typing
+              style={[styles.cellIcon, style, column.style]}
+            />
+          ) : column.type === 'checkbox' ? (
+            <CheckBox
+              key={column.key}
+              style={{ marginLeft: 8 }}
+              value={row.selected || false}
+              onChange={value =>
+                onSelect && onSelect(row[column.key].toString(), value)
+              }
+            />
+          ) : (
+            <Text key={column.key} style={[styles.cell, style, column.style]}>
+              {row[column.key].toString()}
+            </Text>
+          );
+        })}
+      </View>
+    );
+  };
+
   render() {
-    const {
-      columns,
-      data,
-      idAttribute,
-      style,
-      cellStyle,
-      nativeID,
-      onSelect,
-      onSelectAll
-    } = this.props;
+    const { columns, data, style, nativeID, subHeaderData } = this.props;
 
     return (
       <ScrollView horizontal={true}>
         <View nativeID={nativeID} style={style}>
           <View style={styles.row}>
-            {columns.map(column => {
-              return column.type === 'checkbox' ? (
-                <CheckBox
-                  key={column.key}
-                  style={{ marginLeft: 8 }}
-                  value={data.every(x => x.selected) || false}
-                  onChange={value => onSelectAll && onSelectAll(value)}
-                />
-              ) : (
-                <Text
-                  key={column.key}
-                  style={[
-                    styles.cell,
-                    styles.cellHeader,
-                    column.style,
-                    column.headerStyle
-                  ]}
-                >
-                  {column.title}
-                </Text>
-              );
-            })}
+            {columns.map(column => this.renderHeaderColumn(column))}
           </View>
-          {data.map((row, rowIndex) => (
-            <View
-              key={row[idAttribute].toString()}
-              style={[
-                styles.row,
-                {
-                  backgroundColor:
-                    rowIndex % 2 === 0
-                      ? theme.backgroundColor2
-                      : theme.backgroundColor1
-                }
-              ]}
-            >
-              {columns.map(column => {
-                const style =
-                  cellStyle &&
-                  cellStyle[row[idAttribute].toString()] &&
-                  cellStyle[row[idAttribute].toString()][column.key];
-
-                return column.type === 'icon' ? (
-                  <FontAwesome
-                    key={column.key}
-                    name={row[column.key].toString()}
-                    // @ts-ignore : The component uses a different version of typing
-                    style={[styles.cellIcon, style, column.style]}
-                  />
-                ) : column.type === 'checkbox' ? (
-                  <CheckBox
-                    key={column.key}
-                    style={{ marginLeft: 8 }}
-                    value={row.selected || false}
-                    onChange={value =>
-                      onSelect && onSelect(row[column.key].toString(), value)
-                    }
-                  />
-                ) : (
-                  <Text
-                    key={column.key}
-                    style={[styles.cell, style, column.style]}
-                  >
-                    {row[column.key].toString()}
-                  </Text>
-                );
-              })}
+          {subHeaderData && (
+            <View style={styles.row}>
+              {subHeaderData.map(column => this.renderHeaderColumn(column))}
             </View>
-          ))}
+          )}
+          {data.map((row, rowIndex) => this.renderDataTable(row, rowIndex))}
         </View>
       </ScrollView>
     );
